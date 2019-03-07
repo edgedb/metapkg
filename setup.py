c09bc3605e8d74ed305d722487ccd1cb5564247f
Elvis Pranskevichus <elvis@magic.io>
2019-03-07 15:22:36 -0500

    Bump distro dep

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         ]
     },
     install_requires=[
-        'distro~=1.3.0',
+        'distro~=1.4.0',
         'requests~=2.19.0',
         'poetry~=0.11.2',
         'distlib~=0.2.7',
