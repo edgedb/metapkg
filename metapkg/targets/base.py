edgedb/metapkg

commit d42ac9d1abb13d2b8141424389510ddaa4f55e0f

Fix sed command definition regression from the previous patch
Also, take the opportunity to clean things up somewhat to make these
bugs less likely via having fewer places where commands are defined.

metapkg/targets/base.py
@@ -549,10 +549,27 @@ class Build:
         self.prepare()
         self.build()
 
-    def prepare(self) -> None:
+    def define_tools(self) -> None:
+        # Undefining MAKELEVEL is required because
+        # some package makefiles have
+        # conditions on MAKELEVEL.
+        self._system_tools["make"] = "env -u MAKELEVEL make -j{}".format(
+            os.cpu_count()
+        )
+        self._system_tools["cp"] = "cp"
+        self._system_tools["cargo"] = "cargo"
+        self._system_tools["python"] = "python3"
+        self._system_tools["install"] = "install"
+        self._system_tools["patch"] = "patch"
+        self._system_tools["useradd"] = "useradd"
+        self._system_tools["groupadd"] = "groupadd"
+        self._system_tools["sed"] = "sed"
         self._system_tools["make"] = "make"
         self._system_tools["bash"] = "/bin/bash"
         self._system_tools["find"] = "find"
+
+    def prepare(self) -> None:
+        self.define_tools()
 
     def build(self) -> None:
         raise NotImplementedError

metapkg/targets/deb/build.py
@@ -25,20 +25,6 @@ class Build(targets.Build):
         self._pkgroot = self._droot / self._root_pkg.name_slot
         self._srcroot = self._pkgroot / self._root_pkg.name_slot
         self._debroot = self._srcroot / "debian"
-
-        # Undefining MAKELEVEL is required because debian/rules
-        # is a Makefile, and some package makefiles have
-        # conditions on MAKELEVEL.
-        self._system_tools["make"] = "env -u MAKELEVEL make -j{}".format(
-            os.cpu_count()
-        )
-        self._system_tools["cargo"] = "cargo"
-        self._system_tools["python"] = "python3"
-        self._system_tools["cp"] = "cp"
-        self._system_tools["install"] = "install"
-        self._system_tools["useradd"] = "useradd"
-        self._system_tools["groupadd"] = "groupadd"
-
         self._artifactroot = pathlib.Path("_artifacts")
         self._buildroot = self._artifactroot / "build"
         self._tmproot = self._artifactroot / "tmp"

metapkg/targets/generic/build.py
@@ -23,21 +23,6 @@ class Build(targets.Build):
         self._pkgroot = self._droot / self._root_pkg.name_slot
         self._srcroot = self._pkgroot / self._root_pkg.name_slot
 
-        # Undefining MAKELEVEL is required because
-        # some package makefiles have
-        # conditions on MAKELEVEL.
-        self._system_tools["make"] = "env -u MAKELEVEL make -j{}".format(
-            os.cpu_count()
-        )
-        self._system_tools["cp"] = "cp"
-        self._system_tools["cargo"] = "cargo"
-        self._system_tools["python"] = "python3"
-        self._system_tools["install"] = "install"
-        self._system_tools["patch"] = "patch"
-        self._system_tools["useradd"] = "useradd"
-        self._system_tools["groupadd"] = "groupadd"
-        self._system_tools["sed"] = "sed"
-
         self._artifactroot = pathlib.Path("_artifacts")
         self._buildroot = self._artifactroot / "build"
         self._tmproot = self._artifactroot / "tmp"
@@ -427,14 +412,3 @@ class Build(targets.Build):
                             an / pathlib.Path(file).relative_to(image_root)
                         ),
                     )
-
-
-class GenericMacOSBuild(Build):
-    def prepare(self) -> None:
-        super().prepare()
-        self._system_tools["bash"] = "/usr/local/bin/bash"
-        self._system_tools["make"] = (
-            "env -u MAKELEVEL /usr/local/bin/gmake "
-            f"-j{os.cpu_count()} SHELL=/usr/local/bin/bash"
-        )
-        self._system_tools["sed"] = "/usr/local/bin/gsed"

metapkg/targets/macos/build.py
@@ -11,8 +11,8 @@ from metapkg import tools
 
 
 class GenericBuild(generic.Build):
-    def prepare(self) -> None:
-        super().prepare()
+    def define_tools(self) -> None:
+        super().define_tools()
         self._system_tools["bash"] = "/usr/local/bin/bash"
         self._system_tools["make"] = (
             "env -u MAKELEVEL /usr/local/bin/gmake "

metapkg/targets/rpm/build.py
@@ -29,15 +29,6 @@ class Build(targets.Build):
         self._buildroot = pathlib.Path("BUILD")
         self._tmproot = pathlib.Path("TEMP")
         self._installroot = pathlib.Path("INSTALL")
-
-        self._system_tools["make"] = f"make -j{os.cpu_count()}"
-        self._system_tools["cargo"] = "cargo"
-        self._system_tools["python"] = "python3"
-        self._system_tools["cp"] = "cp"
-        self._system_tools["install"] = "install"
-        self._system_tools["useradd"] = "useradd"
-        self._system_tools["groupadd"] = "groupadd"
-
         self._bin_shims = self._root_pkg.get_bin_shims(self)
 
     def get_source_abspath(self) -> pathlib.Path:

metapkg/targets/win/build.py
@@ -3,8 +3,8 @@ from metapkg.targets import generic
 
 
 class Build(generic.Build):
-    def prepare(self) -> None:
-        super().prepare()
+    def define_tools(self) -> None:
+        super().define_tools()
         # "realbash" below is to circumvent a dubious practice
         # of Windows intercepting bare invocations of "bash" to mean
         # "WSL", since make runs its shells using bare names even

commit b28f6a1ddc07b4bdb3e9c52a397d0c03da45b760

Fix definition of `make`
Broken in d42ac9d1abb1.

metapkg/targets/base.py
@@ -541,7 +541,6 @@ class Build:
         self._system_tools["useradd"] = "useradd"
         self._system_tools["groupadd"] = "groupadd"
         self._system_tools["sed"] = "sed"
-        self._system_tools["make"] = "make"
         self._system_tools["bash"] = "/bin/bash"
         self._system_tools["find"] = "find"
 