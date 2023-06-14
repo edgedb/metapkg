edgedb/metapkg

commit d7860689bb133539e1722ef66c7cce9e35bb32cf

generic: Do not mix the sources of the root package and deps
This confuses Cargo and its understanding of workspaces.  This was
masked by an earlier bug that was causing the root package to be treated
as a dependency path-wise.

metapkg/targets/generic/build.py
@@ -65,7 +65,7 @@ class Build(targets.Build):
             return pathlib.Path("..") / path
         elif relative_to == "pkgsource":
             if package is not None and package.name == self.root_package.name:
-                return pathlib.Path(path)
+                return pathlib.Path("..") / path
             else:
                 return pathlib.Path("..") / ".." / path
         elif relative_to == "pkgbuild":
@@ -120,7 +120,10 @@ class Build(targets.Build):
         relative_to: targets.Location = "sourceroot",
     ) -> pathlib.Path:
         if package.name == self.root_package.name:
-            return self.get_dir(".", relative_to=relative_to)
+            return self.get_dir(
+                self.root_package.name,
+                relative_to=relative_to,
+            )
         else:
             return self.get_dir(
                 pathlib.Path("thirdparty") / package.name,

commit 8784ced779cead42da38b4ed4aab314c432971bd

Fix patching of root packages on generic target
A regression from d7860689bb13

metapkg/targets/generic/build.py
@@ -202,7 +202,7 @@ class Build(targets.Build):
         proot = self.get_patches_root(relative_to="fsroot")
         patch_cmd = shlex.split(self.sh_get_command("patch"))
         dep_root = self.get_dir("thirdparty", relative_to="fsroot")
-        my_root = self.get_dir("..", relative_to="fsroot")
+        my_root = self.get_source_abspath()
         for pkgname, patchname in self._patches:
             sroot = my_root if pkgname == self.root_package.name else dep_root
             patch = proot / patchname