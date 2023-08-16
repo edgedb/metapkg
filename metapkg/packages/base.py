edgedb/metapkg

commit b7ba95e0c3d27ee12ecdcd083482ee2d5a1fb150

Add support for flat package layout
The current `PackageFileLayout.FLAT` is actually a single binary layout,
so rename it as such, and add a proper `FLAT` mode where all installed
files are archived at the top level.  Useful for packages with just a
few files, e.g. a binary and a config.

metapkg/packages/base.py
@@ -60,6 +60,7 @@ class PackageFileLayout(enum.IntEnum):
 
     REGULAR = enum.auto()
     FLAT = enum.auto()
+    SINGLE_BINARY = enum.auto()
 
 
 @dataclasses.dataclass

metapkg/targets/_helpers/copy-tree.py
@@ -9,6 +9,7 @@ import pathlib
 import platform
 import shutil
 import stat
+import sys
 
 
 logger = logging.getLogger("copy-tree")
@@ -34,6 +35,11 @@ def parse_args() -> argparse.Namespace:
         help="Optional list of files to copy from the source directory.",
     )
     parser.add_argument(
+        "--flatten",
+        help="Copy all files to the top directory.",
+        action="store_true",
+    )
+    parser.add_argument(
         "-v",
         "--verbose",
         help="Show information on each file copied, directory made, etc.",
@@ -42,7 +48,18 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main(src: str, dest: str, *, files_from: Optional[str]) -> None:
+def die(msg: str) -> NoReturn:
+    logger.error(msg)
+    sys.exit(1)
+
+
+def main(
+    src: str,
+    dest: str,
+    *,
+    files_from: Optional[str],
+    flatten: bool,
+) -> None:
     dest = ensure_destination(src, dest)
     all_files = list(ensure_relative(get_paths_in(src), src))
 
@@ -54,12 +71,12 @@ def main(src: str, dest: str, *, files_from: Optional[str]) -> None:
             f"Using file list in {p} with {len(relative_files)} entries"
         )
         warn_about_excluded_files(included=relative_files, all_files=all_files)
-        copy_files(src, dest, relative_files)
+        copy_files(src, dest, relative_files, flatten=flatten)
     else:
         logger.info(
             f"No file list given, copying all {len(all_files)} entries"
         )
-        copy_files(src, dest, all_files)
+        copy_files(src, dest, all_files, flatten=flatten)
 
 
 def ensure_destination(src: str, dest: str) -> str:
@@ -115,16 +132,42 @@ def ensure_relative(files: Iterable[str], root: str) -> Iterator[str]:
         logger.error(f"File in file list doesn't exist: {path}")
 
 
-def copy_files(src: str, dest: str, files: Iterable[str]) -> None:
+def copy_files(
+    src: str,
+    dest: str,
+    files: Iterable[str],
+    *,
+    flatten: bool,
+) -> None:
     """Copy files listed in `files` from `src` to `dest`.
 
     Paths in `files` must be relative.
     """
     src_dir = pathlib.Path(src)
     dest_dir = pathlib.Path(dest)
-    for file in files:
-        path_from = src_dir / file
-        path_to = dest_dir / file
+    basenames = {}
+    for src_file in files:
+        path_from = src_dir / src_file
+
+        if flatten:
+            if path_from.is_dir():
+                logger.info(f"Skipping directory {src_file} due to --flatten")
+                continue
+            if path_from.is_symlink():
+                logger.info(f"Skipping symlink {src_file} due to --flatten")
+                continue
+
+            basename = path_from.name
+            if basename in basenames:
+                die(
+                    f"`copy-tree --flatten` encountered a duplicate file name:"
+                    f" {src_file} and {basenames[basename]}")
+            basenames[basename] = src_file
+            dest_file = basename
+        else:
+            dest_file = src_file
+
+        path_to = dest_dir / dest_file
         if path_from.is_dir():
             if path_to.is_dir():
                 logger.warning(f"Directory {path_to} already exists")
@@ -214,4 +257,4 @@ if __name__ == "__main__":
         level=logging.INFO if args.verbose else logging.WARNING,
         format="%(asctime)s - %(levelname)s: %(message)s",
     )
-    main(args.src, args.dest, files_from=args.files_from)
+    main(args.src, args.dest, files_from=args.files_from, flatten=args.flatten)

metapkg/packages/go.py
@@ -15,7 +15,7 @@ class BundledGoPackage(base.BuildSystemMakePackage):
         self,
         build: targets.Build,
     ) -> base.PackageFileLayout:
-        return base.PackageFileLayout.FLAT
+        return base.PackageFileLayout.SINGLE_BINARY
 
     def get_configure_script(self, build: targets.Build) -> str:
         sdir = shlex.quote(
@@ -28,13 +28,16 @@ class BundledGoPackage(base.BuildSystemMakePackage):
         return ""
 
     def get_build_install_script(self, build: targets.Build) -> str:
-        if self.get_package_layout(build) is not base.PackageFileLayout.FLAT:
+        if (
+            self.get_package_layout(build)
+            is not base.PackageFileLayout.SINGLE_BINARY
+        ):
             script = super().get_build_install_script(build)
         else:
             script = ""
         installdest = build.get_install_dir(self, relative_to="pkgbuild")
         outdir = self.get_binary_output_dir()
-        bindir = build.get_install_path("systembin").relative_to("/")
+        bindir = build.get_install_path("bin").relative_to("/")
         dest = installdest / bindir / self.name
         return textwrap.dedent(
             f"""\
@@ -46,7 +49,7 @@ class BundledGoPackage(base.BuildSystemMakePackage):
 
     def get_file_install_entries(self, build: targets.Build) -> list[str]:
         entries = list(super().get_file_install_entries(build))
-        entries.append(f"{{systembindir}}/{self.name}{{exesuffix}}")
+        entries.append(f"{{bindir}}/{self.name}{{exesuffix}}")
         return entries
 
 

metapkg/targets/generic/build.py
@@ -310,6 +310,10 @@ class Build(targets.Build):
             "trim-install", relative_to="sourceroot"
         )
         copy_tree = self.sh_get_command("copy-tree", relative_to="sourceroot")
+        layout = pkg.get_package_layout(self)
+        flatten = (
+            "--flatten" if layout is packages.PackageFileLayout.FLAT else ""
+        )
 
         return textwrap.dedent(
             f"""
@@ -326,7 +330,10 @@ class Build(targets.Build):
 
             {extras_script} >> "{temp_dir}/install.list"
 
-            {copy_tree} -v --files-from="{temp_dir}/install.list" \\
+            {copy_tree} \\
+                --verbose \\
+                --files-from="{temp_dir}/install.list" \\
+                {flatten} \\
                 "{install_dir}/" "{image_root}/"
 
             popd >/dev/null
@@ -527,50 +534,57 @@ class Build(targets.Build):
             an = f"{an}_{self._revision}"
         archives = self.get_intermediate_output_dir()
         archives_abs = self.get_intermediate_output_dir(relative_to="fsroot")
+        layout = pkg.get_package_layout(self)
 
-        if pkg.get_package_layout(self) is packages.PackageFileLayout.FLAT:
-            if len(files) == 1:
-                fn = files[0]
-                dest = f"{archives / an}{fn.suffix}"
-                tools.cmd(
-                    "cp",
-                    image_root / fn,
-                    dest,
-                    cwd=src_root,
-                )
-
-                mime = magic.from_file(str(src_root / dest), mime=True)
-
-                tools.cmd(
-                    "zstd",
-                    "-19",
-                    f"{an}{fn.suffix}",
-                    cwd=archives_abs,
-                )
-
-                installrefs = [
-                    f"{an}{fn.suffix}",
-                    f"{an}{fn.suffix}.zst",
-                ]
-
-                installrefs_ct = {
-                    f"{an}{fn.suffix}": {
-                        "type": mime,
-                        "encoding": "identity",
-                        "suffix": fn.suffix,
-                    },
-                    f"{an}{fn.suffix}.zst": {
-                        "type": mime,
-                        "encoding": "zstd",
-                        "suffix": ".zst",
-                    },
-                }
-            else:
+        if layout is packages.PackageFileLayout.SINGLE_BINARY:
+            if len(files) != 1:
                 raise AssertionError(
                     "Single-file package build produced multiple files!"
                 )
+
+            fn = files[0]
+            dest = f"{archives / an}{fn.suffix}"
+            tools.cmd(
+                "cp",
+                image_root / fn,
+                dest,
+                cwd=src_root,
+            )
+
+            mime = magic.from_file(str(src_root / dest), mime=True)
+
+            tools.cmd(
+                "zstd",
+                "-19",
+                f"{an}{fn.suffix}",
+                cwd=archives_abs,
+            )
+
+            installrefs = [
+                f"{an}{fn.suffix}",
+                f"{an}{fn.suffix}.zst",
+            ]
+
+            installrefs_ct = {
+                f"{an}{fn.suffix}": {
+                    "type": mime,
+                    "encoding": "identity",
+                    "suffix": fn.suffix,
+                },
+                f"{an}{fn.suffix}.zst": {
+                    "type": mime,
+                    "encoding": "zstd",
+                    "suffix": ".zst",
+                },
+            }
+
         else:
-            src = image_root / self.get_full_install_prefix().relative_to("/")
+            if layout is packages.PackageFileLayout.FLAT:
+                src = image_root
+            else:
+                prefix = self.get_full_install_prefix().relative_to("/")
+                src = image_root / prefix
+
             tarball = f"{an}.tar"
             tools.cmd(
                 self.sh_get_command("tar"),

commit 78a47063f7e18394fd1622d3dada4475d45ab52c

Move `get_package_layout` to `BasePackage`
It is now referenced from `_get_package_install_script` in generic
builds, which were broken in b7ba95e0c3.

metapkg/packages/base.py
@@ -247,6 +247,9 @@ class BasePackage(poetry_pkg.Package):
             scriptfile_name, pyscript, relative_to="pkgbuild"
         )
 
+    def get_package_layout(self, build: targets.Build) -> PackageFileLayout:
+        return PackageFileLayout.REGULAR
+
 
 BundledPackage_T = TypeVar("BundledPackage_T", bound="BundledPackage")
 
@@ -815,9 +818,6 @@ class BundledPackage(BasePackage):
     def get_bin_shims(self, build: targets.Build) -> dict[str, str]:
         return self.read_support_files(build, "shims/*")
 
-    def get_package_layout(self, build: targets.Build) -> PackageFileLayout:
-        return PackageFileLayout.REGULAR
-
     def __repr__(self) -> str:
         return "<BundledPackage {}>".format(self.unique_name)
 
@@ -1026,7 +1026,8 @@ class BundledCPackage(BuildSystemMakePackage):
             rel_path = f'$(pwd)/"{path}"'
 
             dep_ldflags = build.sh_get_bundled_shlib_ldflags(
-                pkg, relative_to="pkgbuild")
+                pkg, relative_to="pkgbuild"
+            )
 
             if var_prefix:
                 build.sh_append_flags(
@@ -1121,7 +1122,7 @@ class BundledCMesonPackage(BundledCPackage):
             conf_args["--libdir"] = build.get_install_path("lib")
         if "--includedir" not in args:
             conf_args["--includedir"] = build.get_install_path("include")
-        env_args = {}
+        env_args: dict[str, str | pathlib.Path | None] = {}
         build.sh_append_run_time_ldflags(env_args, self)
         env_args = build.sh_append_global_flags(env_args)
         conf = build.sh_format_command(path, conf_args, force_args_eq=True)
@@ -1166,6 +1167,7 @@ class BundledCMesonPackage(BundledCPackage):
         )
 
         return script
+
 
 _semver_phase_spelling_map = {
     poetry_pep440_segments.RELEASE_PHASE_ID_ALPHA: "alpha",

metapkg/targets/_helpers/copy-tree.py
@@ -161,7 +161,8 @@ def copy_files(
             if basename in basenames:
                 die(
                     f"`copy-tree --flatten` encountered a duplicate file name:"
-                    f" {src_file} and {basenames[basename]}")
+                    f" {src_file} and {basenames[basename]}"
+                )
             basenames[basename] = src_file
             dest_file = basename
         else: