edgedb/metapkg

commit 5af5bee3a438c8b6ff46030686e222caa772f191

Make sure install deps and build deps are consistent (#25)
Extra constraints in build dependencies might alter the original
dependency list, so re-run the original resolution with versions of
dependencies pinned to those found in the build resolution.

metapkg/commands/build.py
@@ -84,14 +84,80 @@ class Build(base.Command):
         sources = root_pkg.get_sources()
 
         if len(sources) != 1:
-            self.error("Only single-source packages are supported")
+            self.io.write_error_line(
+                "Only single-source packages are supported"
+            )
             return 1
 
+        env = poetry_env.SystemEnv(pathlib.Path(sys.executable))
+        packages, build_pkgs = self._resolve_deps(env, target, root_pkg, [])
+
+        # Build dependency resolution could have changed the
+        # installable dependency list, so we might need to re-run
+        # the non-build-deps resolution.
+        reresolve_deps = self._check_dep_consistency(packages, build_pkgs)
+        if reresolve_deps:
+            packages, build_pkgs = self._resolve_deps(
+                env, target, root_pkg, reresolve_deps
+            )
+
+        # Check again
+        reresolve_deps = self._check_dep_consistency(packages, build_pkgs)
+        if reresolve_deps:
+            self.io.write_error_line(
+                "Unresolveable install-time vs build-time dependency graph. "
+                + "Mismatching dependencies: "
+                + ", ".join(dep.to_pep_508() for dep in reresolve_deps)
+            )
+            return 1
+
+        if keepwork:
+            workdir = tempfile.mkdtemp(prefix="metapkg.")
+        else:
+            tempdir = tempfile.TemporaryDirectory(prefix="metapkg.")
+            workdir = tempdir.name
+
+        os.chmod(workdir, 0o755)
+
+        try:
+            target.build(
+                targets.BuildRequest(
+                    io=self.io,
+                    env=env,
+                    root_pkg=root_pkg,
+                    deps=packages,
+                    build_deps=build_pkgs,
+                    workdir=workdir,
+                    outputdir=destination,
+                    build_source=build_source,
+                    build_debug=build_debug,
+                    revision=revision or "1",
+                    subdist=subdist,
+                    extra_opt=extra_opt,
+                    jobs=jobs or 0,
+                ),
+            )
+        finally:
+            if not keepwork:
+                tempdir.cleanup()
+
+        return 0
+
+    def _resolve_deps(
+        self,
+        env: poetry_env.Env,
+        target: targets.Target,
+        root_pkg: mpkg_base.BundledPackage,
+        extra_deps: list[poetry_dep.Dependency] | None = None,
+    ) -> tuple[list[mpkg_base.BasePackage], list[mpkg_base.BasePackage]]:
         root = project_package.ProjectPackage("__root__", "1")
         root.python_versions = af_python.python_dependency.pretty_constraint
         root.add_dependency(
             poetry_dep.Dependency(root_pkg.name, root_pkg.version)
         )
+        if extra_deps is not None:
+            for extra_dep in extra_deps:
+                root.add_dependency(extra_dep)
         af_repo.bundle_repo.add_package(root)
 
         target_capabilities = target.get_capabilities()
@@ -108,11 +174,12 @@ class Build(base.Command):
         provider = af_repo.Provider(root, repo_pool, self.io, extras=extras)
         resolution = mixology.resolve_version(root, provider)
 
-        env = poetry_env.SystemEnv(pathlib.Path(sys.executable))
-        pkg_map = {}
+        pkg_map: dict[mpkg_base.NormalizedName, mpkg_base.BasePackage] = {}
         graph = {}
         for dep_package in resolution.packages:
-            pkg_map[dep_package.name] = dep_package
+            pkg_map[dep_package.name] = cast(
+                mpkg_base.BasePackage, dep_package
+            )
             deps = {
                 req.name
                 for req in dep_package.requires
@@ -142,7 +209,9 @@ class Build(base.Command):
         pkg_map = {}
         graph = {}
         for dep_package in resolution.packages:
-            pkg_map[dep_package.name] = dep_package
+            pkg_map[dep_package.name] = cast(
+                mpkg_base.BasePackage, dep_package
+            )
             breqs = mpkg_base.get_build_requirements(dep_package)
             deps = {
                 req.name
@@ -198,34 +267,20 @@ class Build(base.Command):
                     build_pkgs[i + 1 : i + 1] = cr_deps
                     break
 
-        if keepwork:
-            workdir = tempfile.mkdtemp(prefix="metapkg.")
-        else:
-            tempdir = tempfile.TemporaryDirectory(prefix="metapkg.")
-            workdir = tempdir.name
+        return packages, build_pkgs
 
-        os.chmod(workdir, 0o755)
+    def _check_dep_consistency(
+        self,
+        packages: list[mpkg_base.BasePackage],
+        build_pkgs: list[mpkg_base.BasePackage],
+    ) -> list[poetry_dep.Dependency]:
+        build_dep_index = {pkg.name: pkg for pkg in build_pkgs}
+        reresolve_deps = []
+        for pkg in packages:
+            build_dep = build_dep_index.get(pkg.name)
+            if build_dep is not None and build_dep.version != pkg.version:
+                reresolve_deps.append(
+                    poetry_dep.Dependency(build_dep.name, build_dep.version)
+                )
 
-        try:
-            target.build(
-                targets.BuildRequest(
-                    io=self.io,
-                    env=env,
-                    root_pkg=root_pkg,
-                    deps=cast(list[mpkg_base.BasePackage], packages),
-                    build_deps=cast(list[mpkg_base.BasePackage], build_pkgs),
-                    workdir=workdir,
-                    outputdir=destination,
-                    build_source=build_source,
-                    build_debug=build_debug,
-                    revision=revision or "1",
-                    subdist=subdist,
-                    extra_opt=extra_opt,
-                    jobs=jobs or 0,
-                ),
-            )
-        finally:
-            if not keepwork:
-                tempdir.cleanup()
-
-        return 0
+        return reresolve_deps

metapkg/packages/__init__.py
@@ -8,6 +8,7 @@ from .base import (
     BundledCMesonPackage,
     PackageFileLayout,
     MetaPackage,
+    NormalizedName,
     canonicalize_name,
     pep440_to_semver,
     semver_pre_tag,
@@ -31,6 +32,7 @@ __all__ = (
     "BundledPythonPackage",
     "BundledRustPackage",
     "BundledAdHocRustPackage",
+    "NormalizedName",
     "canonicalize_name",
     "pep440_to_semver",
     "semver_pre_tag",

metapkg/packages/base.py
@@ -50,6 +50,7 @@ if TYPE_CHECKING:
 get_build_requirements = repository.get_build_requirements
 set_build_requirements = repository.set_build_requirements
 canonicalize_name = packaging.utils.canonicalize_name
+NormalizedName = packaging.utils.NormalizedName
 
 
 class DummyPackage(poetry_pkg.Package):

commit 07ee6956a775de1f2b9ea28b61dea430c2f356df

Fix handling of dependencies enabled by target capabilities
Broken by #25, because the second dependency resolution run would
incorrectly skip dependencies activated in a previous run.

metapkg/commands/build.py
@@ -189,6 +189,8 @@ class Build(base.Command):
         sorter = graphlib.TopologicalSorter(graph)
         packages = [pkg_map[pn] for pn in sorter.static_order()]
 
+        af_repo.bundle_repo.remove_package(root)
+
         # Build a separate package list for build deps.
         build_root = project_package.ProjectPackage("__build_root__", "1")
         build_root.python_versions = (

metapkg/packages/repository.py
@@ -169,10 +169,10 @@ class Provider(poetry_provider.Provider):
         pkg = super().complete_package(package)
 
         for dep in itertools.chain.from_iterable(chain):
-            if not dep.is_activated() and dep.in_extras:
-                if not ({str(e) for e in dep.in_extras} - self._active_extras):
-                    dep.activate()
-                    pkg.package.add_dependency(dep)
+            dep_in_extras = {str(e) for e in dep.in_extras}
+            if not (dep_in_extras - self._active_extras):
+                dep.activate()
+                pkg.package.add_dependency(dep)
 
         return pkg
 